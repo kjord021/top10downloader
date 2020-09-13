9ac6068154640c1b2f28803e6339b662a4cd3bbf
Kyle Jordan <kjord021@fiu.edu>
2020-09-13 08:37:14 -0400

    Add DownloadXML Code

--- app/src/main/java/com/example/top10downloader/MainActivity.java
+++ app/src/main/java/com/example/top10downloader/MainActivity.java
@@ -6,6 +6,13 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -35,7 +42,47 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... strings) {
             Log.d(TAG, "doInBackground: starts with " + strings[0]);
-            return "doInBackground completed.";
+
+            String rssFeed = downloadXML(strings[0]);
+
+            if (rssFeed == null) {
+                Log.e(TAG,"doInBackground: Error downloading");
+            }
+
+            return rssFeed;
+        }
+
+        private String downloadXML(String urlPath) {
+            StringBuilder xmlResult = new StringBuilder();
+
+            try {
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+
+                Log.d(TAG,"downloadXML: The response code was " + response);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charsRead;
+                char[] inputBuffer = new char[500];
+                while(true) {
+                    charsRead = reader.read(inputBuffer);
+                    if(charsRead < 0){
+                        break;
+                    }
+                    if(charsRead > 0) {
+                        xmlResult.append(String.copyValueOf(inputBuffer, 0 , charsRead));
+                    }
+                }
+                reader.close();
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG,"downloadXML: Invalid URL " + e.getMessage());
+            } catch (IOException e) {
+                Log.e(TAG, "downloadXML: IO Exception reading data: " + e.getMessage());
+            }
+            return urlPath;
         }
     }
 }
